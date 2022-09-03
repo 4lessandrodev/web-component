0eae014e1e0b82fc3c9722e64e8ffe4ecc06a422
4lessandrodev <alessandroadm@live.com>
2022-09-03 16:02:41 -0300

    fix(component): change component

--- Title.spec.tsx
+++ Title.spec.tsx
@@ -4,7 +4,7 @@ import { screen, render } from '@testing-library/react';
 describe('Title', () => {
 
 	it('should render small with success', async () => {
-		const component = render(<Component size="SMALL" value="Small Title"/>);
+		render(<Component size="SMALL" value="Small Title"/>);
 		const element = screen.getByText(/Small Title/i);
 		expect(element).toBeInTheDocument();
 		expect(element).toHaveTextContent("Small Title");
@@ -12,7 +12,7 @@ describe('Title', () => {
 	});
 
 	it('should render medium with success', async () => {
-		const component = render(<Component size="MEDIUM" value="Medium Title"/>);
+		render(<Component size="MEDIUM" value="Medium Title"/>);
 		const element = screen.getByText(/Medium Title/i);
 		expect(element).toBeInTheDocument();
 		expect(element).toHaveTextContent("Medium Title");
@@ -20,7 +20,7 @@ describe('Title', () => {
 	});
 
 	it('should render large with success', async () => {
-		const component = render(<Component size="LARGE" value="Large Title"/>);
+		render(<Component size="LARGE" value="Large Title"/>);
 		const element = screen.getByText(/Large Title/i);
 		expect(element).toBeInTheDocument();
 		expect(element).toHaveTextContent("Large Title");
