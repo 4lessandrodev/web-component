cfe31ea6ec486ae42e5a6a51c2af8a3f11244323
4lessandrodev <alessandroadm@live.com>
2022-09-04 17:04:20 -0300

    fix(imports): added jest-dom to test

--- Title.spec.tsx
+++ Title.spec.tsx
@@ -1,3 +1,4 @@
+import '@testing-library/jest-dom';
 import React from 'react';
 import Component from './index';
 import { screen, render } from '@testing-library/react';
@@ -9,7 +10,7 @@ describe('Title', () => {
 		const element = screen.getByText(/Small Title/i);
 		expect(element).toBeInTheDocument();
 		expect(element).toHaveTextContent("Small Title");
-		expect(element.style.fontSize).toBe("1rem");
+		expect(element).toHaveStyle('font-size: 1rem');
 	});
 
 	it('should render medium with success', async () => {
@@ -17,7 +18,7 @@ describe('Title', () => {
 		const element = screen.getByText(/Medium Title/i);
 		expect(element).toBeInTheDocument();
 		expect(element).toHaveTextContent("Medium Title");
-		expect(element.style.fontSize).toBe("1.2rem");
+		expect(element).toHaveStyle('font-size: 1.2rem');
 	});
 
 	it('should render large with success', async () => {
@@ -25,6 +26,6 @@ describe('Title', () => {
 		const element = screen.getByText(/Large Title/i);
 		expect(element).toBeInTheDocument();
 		expect(element).toHaveTextContent("Large Title");
-		expect(element.style.fontSize).toBe("2rem");
+		expect(element).toHaveStyle("font-size: 2rem");
 	});
 });
